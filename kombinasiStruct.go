a433506690273c8bede774827ec298f26d1b285c
vsualzm <ims.ilham@gmail.com>
2023-02-01 18:48:28 +0700

    test

--- kombinasiStruct.go
+++ kombinasiStruct.go
@@ -1,37 +1,37 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-type person struct {
-	name string
-	age  int
-}
+// type person struct {
+// 	name string
+// 	age  int
+// }
 
-func main() {
+// func main() {
 
-	var allStudents = []person{
-		{name: "ilham", age: 21},
-		{name: "ucok", age: 22},
-		{name: "repal", age: 23},
-	}
+// 	var allStudents = []person{
+// 		{name: "ilham", age: 21},
+// 		{name: "ucok", age: 22},
+// 		{name: "repal", age: 23},
+// 	}
 
-	for _, student := range allStudents {
-		fmt.Println(student.name, "age is", student.age)
-	}
+// 	for _, student := range allStudents {
+// 		fmt.Println(student.name, "age is", student.age)
+// 	}
 
-	// anon
+// anon
 
-	// var allStudents = []struct {
-	// 	person
-	// 	grade int
-	// }{
-	// 	{person: person{"wick", 21}, grade: 2},
-	// 	{person: person{"ethan", 22}, grade: 3},
-	// 	{person: person{"bond", 21}, grade: 3},
-	// }
+// var allStudents = []struct {
+// 	person
+// 	grade int
+// }{
+// 	{person: person{"wick", 21}, grade: 2},
+// 	{person: person{"ethan", 22}, grade: 3},
+// 	{person: person{"bond", 21}, grade: 3},
+// }
 
-	// for _, student := range allStudents {
-	// 	fmt.Println(student)
-	// }
+// for _, student := range allStudents {
+// 	fmt.Println(student)
+// }
 
-}
+// }
